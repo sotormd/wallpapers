7ad9c2050dd227e77c761dbd9cc96cf7220c676d
vrm <sotormd@proton.me>
2025-10-12 16:07:01 +0530

    feat: add new wallpapers

--- gen-preview.sh
+++ gen-preview.sh
@@ -1,27 +1,29 @@
-#! /usr/bin/env bash
-
+#!/usr/bin/env bash
 
 set -euo pipefail
-IFS=$'\n'  # read line by line safely
+IFS=$'\n'
 
 WALL_DIR="./wallpapers"
 
 echo "# Preview"
 echo
 
-# pind all png files recursively, sort them
-mapfile -t files < <(find "$WALL_DIR" -type f -name '*.png' | sort)
+# first: png files in wallpapers/nord
+mapfile -t nord_files < <(find "$WALL_DIR/nord" -type f -name '*.png' | sort)
+
+# then: png files in other directories
+mapfile -t other_files < <(find "$WALL_DIR" -type f -name '*.png' ! -path "$WALL_DIR/nord/*" | sort)
+
+# combine both arrays
+files=("${nord_files[@]}" "${other_files[@]}")
 
 for file in "${files[@]}"; do
-    # remove leading ./ and WALL_DIR/
     relpath="${file#./}"
     relpath="${relpath#$WALL_DIR/}"
 
-    # convert path to nix attribute format: replace / with .
     attr="${relpath%.*}"
     attr="${attr//\//.}"
 
-    # output markdown block
     printf '`%s`\n' "$attr"
     printf '![](%s)\n\n' "$file"
 done
